0108b10765f41be0b28810c3e5ce0d9f13919ebb
Dwa Meizadewa <infamous0192@gmail.com>
2021-10-13 10:50:46 +0800

    Membuat bole bergerak secara random keluar screen

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        PushBall();
+    }
+
+    void PushBall()
+    {
+        rb2d.AddForce(new Vector2(Random.Range(-20, 20), Random.Range(-20, 20)));
+    }
+}
